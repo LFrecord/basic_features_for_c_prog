1d814cd416438d783d23a56a1a87faff8980a384
lfrecord <khtr24u465@student.bmstu.ru>
2025-12-19 23:18:34 +0300

    Vector implementation was added with at and insert funcs
    Funcs:
    int at(vector, int);
    int insert(vector, void*, size_t);
    were added to file vector.c
    These looks like same named methods of vector in cpp, but they are a
    some changed.
    Func "at" needs index in integer type, because it can use negative
    indexes. Func "insert" cat use as lange as needed index, new element
    will be written to the end of vector.

--- vla_vector/inc/vector.h
+++ vla_vector/inc/vector.h
@@ -4,7 +4,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define vector struct __vector_t__*
+/**
+ * Тип данных, реализующий массив динамической длины (вектор)
+ * 
+ */
+#define vector vector_t
 #define BASE_VECTOR_SIZE 20
 #define VECTOR_INCREASER 5
 #define NO_VECTOR NULL
@@ -18,18 +22,81 @@ enum vector_return_codes
     VECTOR_REALLOC_ERR
 };
 
-struct __vector_t__
-{
-    void *data;
-    size_t max_elems;
-    size_t writen_elems;
-    size_t elem_size;
-};
+typedef struct __vector_type__ *vector_t;
 
+/**
+ * Инициализирует вектор
+ * \param[in,out] container - Описатель вектора
+ * \param[in] elem_size - Размер сохраняемого элемента
+ * 
+ * \return - VECTOR_POS_CASE или код ошибки
+ */
 int init_vector(vector *container, size_t elem_size);
+
+/**
+ * Освобождает вектор
+ * \param[in,out] container - Описатель вектора
+ * 
+ * \note - После освобождения памяти определитель становится равен NO_VECTOR
+ * 
+ * \return - VECTOR_POS_CASE или код ошибки
+ */
 int free_vector(vector *container);
-int push_back_vector(vector container, void *elem);
+
+
+/**
+ * Добавляет элемент в конец вектора
+ * \param[in,out] container - Описатель вектора
+ * \param[in] p_elem - Указатель на объект
+ * 
+ * \return - VECTOR_POS_CASE или код ошибки
+ */
+int push_back_vector(vector container, void *p_elem);
+
+/**
+ * Удаляет элемент из конца вектора
+ * \param[in,out] container - Описатель вектора
+ * 
+ * \note Если вектор пуст, ничего не делает. Случай не возвращает кода ошибки.
+ * 
+ * \return - VECTOR_POS_CASE или код ошибки
+ */
 int pop_back_vector(vector container);
+
+/**
+ * Добавляет элемент в позицию вектора по индексу
+ * \param[in,out] container - Описатель вектора
+ * \param[in] p_elem - Указатель на элемент
+ * \param[in] index - Индекс позиции, на которую будет записан элемент
+ * 
+ * \note Если индекс окажется больше длины вектора, то элемент будет записан в конец
+ * 
+ * \return - VECTOR_POS_CASE или код ошибки
+ */
+int insert_vector(vector container, void *p_elem, size_t index);
+
+
+/**
+ * Возвращает указатель на элемент по индексу
+ * \param[in,out] container - Описатель вектора
+ * \param[in] index - Индекс элемента, указатель на который будет возвращён
+ * 
+ * \note Индекс имеет тип int, те он может быть как положительным, так и отрицательным. Отрицательные индексы возвращают указатели с последнего при -1 итд в
+ * обратном порядке. Ошибочной являются ситуации, когда переданный индекс больше индекса последнего элемента или когда модуль переданного индекса больше
+ * количество записанных элементов.
+ * 
+ * \return - VECTOR_POS_CASE или код ошибки
+ */
+void *at_vector(vector container, int index);
+
+/**
+ * Возвращает длину вектора (количество записанных элементов)
+ * \param[in,out] container - Описатель вектора
+ * 
+ * \note Если вектор не проинициализирован, функция возвращает 0.
+ * 
+ * \return - VECTOR_POS_CASE или код ошибки
+ */
 int size_vector(vector container);
 
 #endif
--- vla_vector/src/vector.c
+++ vla_vector/src/vector.c
@@ -1,11 +1,47 @@
 #include "vector.h"
 
+struct __vector_type__
+{
+    void *data;
+    size_t capacity;
+    size_t elems_amount;
+    size_t size;
+};
+
+static int increase_data(vector container)
+{
+    int rc = (container == NO_VECTOR) ? VECTOR_VAR_ERR : VECTOR_POS_CASE;
+    if (rc == VECTOR_POS_CASE)
+    {
+        void *help_ptr = realloc(container->data, (container->capacity += VECTOR_INCREASER + 1) * container->size);
+        if (help_ptr == NULL)
+            rc = VECTOR_REALLOC_ERR;
+        else
+            container->data = help_ptr;
+    }
+    return rc;
+}
+
+static int decrease_data(vector container)
+{
+    int rc = (container == NO_VECTOR) ? VECTOR_VAR_ERR : VECTOR_POS_CASE;
+    if (rc == VECTOR_POS_CASE)
+    {
+        void *help_ptr = realloc(container->data, (container->capacity -= VECTOR_INCREASER + 1) * container->size);
+        if (help_ptr == NULL)
+            rc = VECTOR_REALLOC_ERR;
+        else
+            container->data = help_ptr;
+    }
+    return rc;
+}
+
 int init_vector(vector *container, size_t elem_size)
 {
     int rc = (container == NULL || elem_size == 0) ? VECTOR_VAR_ERR : VECTOR_POS_CASE;
     if (rc == VECTOR_POS_CASE)
     {
-        *container = malloc(sizeof(struct __vector_t__));
+        *container = malloc(sizeof(struct __vector_type__));
         if (*container == NULL)
             rc = VECTOR_MALLOC_ERR;
     }
@@ -16,9 +52,9 @@ int init_vector(vector *container, size_t elem_size)
             rc = VECTOR_DATA_MALLOC_ERR;
         else
         {
-            (*container)->max_elems = BASE_VECTOR_SIZE;
-            (*container)->elem_size = elem_size;
-            (*container)->writen_elems = 0;
+            (*container)->capacity = BASE_VECTOR_SIZE;
+            (*container)->size = elem_size;
+            (*container)->elems_amount = 0;
         }
     }
     return rc;
@@ -36,41 +72,61 @@ int free_vector(vector *container)
     return rc;
 }
 
-int push_back_vector(vector container, void *elem)
+int push_back_vector(vector container, void *p_elem)
 {
-    int rc = (container == NO_VECTOR || elem == NULL) ? VECTOR_VAR_ERR : VECTOR_POS_CASE;
-    if (rc == VECTOR_POS_CASE && container->max_elems == container->writen_elems)
-    {
-        void *help_ptr = realloc(container->data, (container->max_elems += VECTOR_INCREASER + 1) * container->elem_size);
-        if (help_ptr == NULL)
-            rc = VECTOR_REALLOC_ERR;
-        else
-            container->data = help_ptr;
-    }
+    int rc = (container == NO_VECTOR || p_elem == NULL) ? VECTOR_VAR_ERR : VECTOR_POS_CASE;
+    if (rc == VECTOR_POS_CASE && container->capacity == container->elems_amount)
+        rc = increase_data(container);
     if (rc == VECTOR_POS_CASE)
-        memcpy((void*)((char*)container->data + container->elem_size * (container->writen_elems)++), elem, container->elem_size);
+        memcpy((void*)((char*)container->data + container->size * (container->elems_amount)++), p_elem, container->size);
     return rc;
 }
 
 int pop_back_vector(vector container)
 {
     int rc = (container == NO_VECTOR) ? VECTOR_VAR_ERR : VECTOR_POS_CASE;
-    if (rc == VECTOR_POS_CASE && container->writen_elems != 0)
+    if (rc == VECTOR_POS_CASE && container->elems_amount != 0)
     {
-        (container->writen_elems)--;
-        if (container->max_elems - container->writen_elems > VECTOR_INCREASER)
+        (container->elems_amount)--;
+        if (container->capacity - container->elems_amount > VECTOR_INCREASER)
+            rc = decrease_data(container);
+    }
+    return rc;
+}
+
+int insert_vector(vector container, void *p_elem, size_t index)
+{
+    int rc = (container == NO_VECTOR || p_elem == NULL) ? VECTOR_POS_CASE : VECTOR_POS_CASE;
+    if (rc == VECTOR_POS_CASE && container->elems_amount == container->capacity)
+        rc = increase_data(container);
+    if (rc == VECTOR_POS_CASE)
+    {
+        if (index >= container->elems_amount)
+            rc = push_back_vector(container, p_elem);
+        else
         {
-            void *help_ptr = realloc(container->data, (container->max_elems -= VECTOR_INCREASER + 1) * container->elem_size);
-            if (help_ptr == NULL)
-                rc = VECTOR_REALLOC_ERR;
-            else
-                container->data = help_ptr;
+            for (size_t i = ++(container->elems_amount), size = container->size; i > index; i--)
+                memcpy((char*)container->data + size * i, (char*)container->data + size * (i - 1), size);
+            memcpy((char*)container->data + container->size * index, p_elem, container->size);
         }
     }
     return rc;
 }
 
+void *at_vector(vector container, int index)
+{
+    void *rp = NULL;
+    if (container != NO_VECTOR)
+    {
+        if (index >= 0 && (size_t)(index) < container->elems_amount)
+            rp = (void*)((char*)container->data + container->size * index);
+        else if (index < 0 && (size_t)(-1 * index) <= container->elems_amount)
+            rp = (void*)((char*)container->data + container->size * (container->elems_amount + index));
+    }
+    return rp;
+}
+
 int size_vector(vector container)
 {
-    return (container == NO_VECTOR) ? 0 : container->writen_elems;
+    return (container == NO_VECTOR) ? 0 : container->elems_amount;
 }
